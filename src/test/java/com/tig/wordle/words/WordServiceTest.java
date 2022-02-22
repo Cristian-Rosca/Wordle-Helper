286ff04539bc9f9c2932be76efc21477aec688f4
Suad <Suadali97@hotmail.com>
2022-02-22 20:55:57 +0000

    add canGetTopWordsTest

--- src/test/java/com/tig/wordle/words/WordServiceTest.java
+++ src/test/java/com/tig/wordle/words/WordServiceTest.java
@@ -106,4 +106,34 @@ public class WordServiceTest {
         Word expected = testWord1;
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void canGetTopWords() {
+        // Given
+        Word testWord1 = new Word(1, "hello", 0.25, 2.0);
+        Word testWord2 = new Word(2, "image", 0.25, 1.0);
+        Word testWord3 = new Word(3, "opens", 0.25, 0.5);
+        Word testWord4 = new Word(4, "fares", 0.25, 0.25);
+        List<Word> allWords = new ArrayList<>();
+        allWords.add(testWord1);
+        allWords.add(testWord2);
+        allWords.add(testWord3);
+        allWords.add(testWord4);
+
+
+        List<Word> topThreeWords = new ArrayList<>();
+        topThreeWords.add(testWord1);
+        topThreeWords.add(testWord2);
+        topThreeWords.add(testWord3);
+
+        given(wordDAO.selectTopWords(3)).willReturn(topThreeWords);
+
+        // When
+        List<Word> actual = underTest.getTopWords(3);
+
+        //Then
+
+        List<Word> expected = topThreeWords;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
