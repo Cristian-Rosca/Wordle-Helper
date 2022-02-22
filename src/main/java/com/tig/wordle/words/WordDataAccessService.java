0230ae09389033d7c88ed0f30a495087c6c1ede7
Rachel Kaur <rachel.km@outlook.com>
2022-02-22 11:18:50 +0000

    add selectAllWords method

--- src/main/java/com/tig/wordle/words/WordDataAccessService.java
+++ src/main/java/com/tig/wordle/words/WordDataAccessService.java
@@ -1,12 +1,13 @@
 package com.tig.wordle.words;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository("postgres")
-public class WordDataAccessService implements WordDAO{
+public class WordDataAccessService implements WordDAO {
 
     private JdbcTemplate jdbcTemplate;
 
@@ -14,9 +15,25 @@ public class WordDataAccessService implements WordDAO{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // SelectAllWords method
     @Override
     public List<Word> selectAllWords() {
-        return null;
+        String sql = """
+                SELECT id, word, probability, score
+                FROM original_word_list
+                """;
+
+        RowMapper<Word> wordRowMapper = (rs, rowNum) -> {
+            Word word = new Word(
+                    rs.getInt("id"),
+                    rs.getString("word"),
+                    rs.getDouble("probability"),
+                    rs.getDouble("score")
+            );
+            return word;
+        };
+        List<Word> gameWordList = jdbcTemplate.query(sql, wordRowMapper);
+        return gameWordList;
     }
 
     @Override
