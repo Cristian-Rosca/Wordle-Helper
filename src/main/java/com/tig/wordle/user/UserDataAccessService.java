88b038678b18b445135ffde69d90e143d86a6084
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-02-23 00:33:06 +0000

    added userRowMapper for UserDataAccessService

--- src/main/java/com/tig/wordle/user/UserDataAccessService.java
+++ src/main/java/com/tig/wordle/user/UserDataAccessService.java
@@ -1,6 +1,8 @@
 package com.tig.wordle.user;
 
+import com.tig.wordle.words.Word;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,7 +10,15 @@ import java.util.List;
 @Repository("postgres")
 public class UserDataAccessService implements UserDAO{
     private JdbcTemplate jdbcTemplate;
-
+    private RowMapper<User> userRowMapper = (rs, rowNum) -> {
+        User user = new User(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("username")
+        );
+        return user;
+    };
     public UserDataAccessService(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
