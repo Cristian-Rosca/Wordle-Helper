3043d3baaa3152d92d68c2756d10417135661d2b
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-02-23 15:41:27 +0000

    added AnswerDateTakenException

--- src/main/java/com/tig/wordle/answers/AnswerService.java
+++ src/main/java/com/tig/wordle/answers/AnswerService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,6 +24,18 @@ public class AnswerService {
     }
 
     public Integer addAnswerToTable(Answer answer){
+        // Get the current answers from the table
+        List<Answer> currentAnswers = getAllAnswers();
+        // Empty list for the dates
+        List<LocalDate> dates = new ArrayList<>();
+        // Fill list of dates
+        for (int i = 0; i < currentAnswers.size(); i++) {
+            dates.add(currentAnswers.get(i).getDateOfAnswer());
+        }
+        // Check if our new answer date is included in current list of dates
+        if (dates.contains(answer.getDateOfAnswer())){
+            throw new AnswerDateTakenException("Could not add answer to table as given date already exists");
+        }
         return answerDAO.addAnswerToTable(answer);
     }
 
@@ -33,6 +47,18 @@ public class AnswerService {
     }
 
     public Integer updateAnswerById(Integer id, Answer answer) {
+        // Get the current answers from the table
+        List<Answer> currentAnswers = getAllAnswers();
+        // Empty list for the dates
+        List<LocalDate> dates = new ArrayList<>();
+        // Fill list of dates
+        for (int i = 0; i < currentAnswers.size(); i++) {
+            dates.add(currentAnswers.get(i).getDateOfAnswer());
+        }
+        // Check if our new answer date is included in current list of dates
+        if (dates.contains(answer.getDateOfAnswer())){
+            throw new AnswerDateTakenException("Could not update answer in table as given date already exists");
+        }
         if (getAnswerById(id) == null){
             throw new AnswerNotFoundException("No answer with given id exists");
         }
