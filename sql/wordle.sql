2fc190628934faddfe45f8352ac11ce5c5e0e099
Rachel Kaur <rachel.km@outlook.com>
2022-02-23 10:54:20 +0000

    update sql set-up file

--- sql/wordle.sql
+++ sql/wordle.sql
@@ -1,22 +1,28 @@
+DROP TABLE IF EXISTS original_word_list CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS actual_answers CASCADE;
+
+-- Create original word list table
 CREATE TABLE original_word_list (
     id SERIAL PRIMARY KEY,
     word VARCHAR(5),
-    probability numeric,
-    score numeric
+    probability NUMERIC,
+    score NUMERIC
 );
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50), UNIQUE
-    email VARCHAR(50), UNIQUE
-    username VARCHAR(50) UNIQUE 
-);
-
+-- Insert data into original word list table
 COPY original_word_list (word, probability, score) FROM
 '/Users/oleksiysmola/IdeaProjects/Wordle-Helper/initialcalculations.txt' DELIMITER ',' CSV;
 
+-- Create users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE,
+    email VARCHAR(50) UNIQUE,
+    username VARCHAR(50) UNIQUE
+);
 
---Actual answers table--
+-- Create actual answers table--
 CREATE TABLE actual_answers (
     id SERIAL PRIMARY KEY,
     date_of_given_answer DATE UNIQUE,
@@ -24,5 +30,6 @@ CREATE TABLE actual_answers (
     machine_guesses INT
 );
 
+-- Insert actual answers and respective dates data into actual answers table
 COPY actual_answers (date_of_given_answer, actual_word) FROM
 '/Users/suad/Downloads/date-word-answer.txt' DELIMITER ',' CSV;
