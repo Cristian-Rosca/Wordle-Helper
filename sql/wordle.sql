25170ccf6fe914b0c1897322aaabfbc7b100e963
Suad <Suadali97@hotmail.com>
2022-02-23 10:18:05 +0000

    add table for actual answers and related file

--- sql/wordle.sql
+++ sql/wordle.sql
@@ -14,3 +14,15 @@ CREATE TABLE users (
 
 COPY original_word_list (word, probability, score) FROM
 '/Users/oleksiysmola/IdeaProjects/Wordle-Helper/initialcalculations.txt' DELIMITER ',' CSV;
+
+
+--Actual answers table--
+CREATE TABLE actual_answers (
+    id SERIAL PRIMARY KEY,
+    date_of_given_answer date UNIQUE,
+    actual_word VARCHAR(5),
+    machine_guesses int
+);
+
+COPY actual_answers (dateOfGivenAnswer, actualword) FROM
+'/Users/suad/Downloads/date-word-answer.txt' DELIMITER ',' CSV;
--- src/main/java/com/tig/wordle/answers/AnswerDataAccessService.java
+++ src/main/java/com/tig/wordle/answers/AnswerDataAccessService.java
@@ -2,9 +2,10 @@ package com.tig.wordle.answers;
 
 import com.tig.wordle.user.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository("postgres")
 public class AnswerDataAccessService implements AnswerDAO{
     private JdbcTemplate jdbcTemplate;
     public AnswerDataAccessService(JdbcTemplate jdbcTemplate){
