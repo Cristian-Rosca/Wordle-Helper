d8339f320723fa5ce27c5c127e699b5a8468d18b
Suad <Suadali97@hotmail.com>
2022-02-23 15:23:09 +0000

    add userNotFoundException

--- src/main/java/com/tig/wordle/user/UserService.java
+++ src/main/java/com/tig/wordle/user/UserService.java
@@ -12,7 +12,7 @@ public class UserService {
     private UserDAO userDAO;
 
 
-    public UserService(@Qualifier("user") UserDAO userDAO)  {
+    public UserService(@Qualifier("user") UserDAO userDAO) {
         this.userDAO = userDAO;
     }
 
@@ -26,74 +26,54 @@ public class UserService {
     }
 
     public User selectUserByID(Integer userId) {
-        //redundant?
-        if (userId == null){
-            throw new IllegalStateException("ID cannot be null");
-        }
-
-
         boolean exists = doesUserWithIdExists(userId);
-        if(exists==false) {
-            throw new IllegalStateException("User with ID " + userId + " does not exist");
+        if (exists == false) {
+            throw new UserNotFoundException("User with ID " + userId + " does not exist");
         }
         User user = userDAO.getUserById(userId);
         return user;
-
-
     }
 
-    public List<User> getAllUsers () {
+    public List<User> getAllUsers() {
         return userDAO.getAllUsers();
     }
 
     public Integer addUserToTable(User user) {
-        if(user.getName() != null
+        if (user.getName() != null
                 && user.getUserName() != null
                 && user.getEmail() != null
                 && user.getName() != ""
                 && user.getUserName() != ""
-                && user.getEmail() != "" ) {
-                return userDAO.addUserToTable(user);
-        }
-        else {
+                && user.getEmail() != "") {
+            return userDAO.addUserToTable(user);
+        } else {
             throw new InputMissingException("Invalid entry. Fields cannot be empty");
         }
     }
 
-    public Integer deleteUserById(Integer userId){
-        //redundant?
-        if (userId == null){
-            throw new IllegalStateException("Id cannot be null");
-        }
-
-
+    public Integer deleteUserById(Integer userId) {
         boolean exists = doesUserWithIdExists(userId);
-        if(exists==false) {
-            throw new IllegalStateException("User with ID " + userId + " does not exist");
+        if (exists == false) {
+            throw new UserNotFoundException("User with ID " + userId + " does not exist");
         }
-
         int result = userDAO.deleteUserById(userId);
-
         return result;
     }
 
-//Better validations!!! check that all fields are entered
+
     public Integer updateUserByID(Integer userId, User updatedUser) {
         boolean exists = doesUserWithIdExists(userId);
-        if(exists==false) {
-            throw new IllegalStateException("User with ID " + userId + " does not exist");
+        if (exists == false) {
+            throw new UserNotFoundException("User with ID " + userId + " does not exist");
         }
-
         if (updatedUser.getName() != null
                 && updatedUser.getEmail() != null
                 && updatedUser.getUserName() != null
                 && updatedUser.getName() != ""
                 && updatedUser.getEmail() != ""
                 && updatedUser.getUserName() != "") {
-            return userDAO.updateUserById(userId,updatedUser);
-        }
-        else throw new InputMissingException("Invalid entry. Fields cannot be empty");
+            return userDAO.updateUserById(userId, updatedUser);
+        } else throw new InputMissingException("Invalid entry. Fields cannot be empty");
 
     }
-
 }
